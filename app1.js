cb18f6bedc76526a2b98a1da10cf0ccbb9e66448
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2020-06-26 14:35:21 +0500

    first commit

---
+++ app1.js
@@ -0,0 +1,46 @@
+// class 1 js
+
+// alert("Hello World!");
+
+// const var2 = "What is variable ?";
+// const var1 = "Variale will allocate space in memory";
+// document.write(var2+"<br>"+var1+"<br>");
+
+// var a = 2+2;
+// alert(a);
+// let b = 2+2;
+// alert(a);
+// const c = 2+2;
+// alert(a);
+
+// let d = 5;
+// let e = 6;
+// let f = d + e ;
+// let g = d - e ;
+// let h = d * e ;
+// let i = d / e ;
+
+// alert(f);
+// alert(g);
+// alert(h);
+// alert(i);
+
+// let j = "Mohtashim";
+// let k = 5;
+// alert(j+" "+k);
+
+// const string1 = "What is string?";
+// const string = "Group of character";
+// document.write(string1+"<br>"+string+"<br>");
+
+
+
+// const conca = "What is concatenation ?";
+// const conca1 = "Merging of two element ." ;
+// document.write(conca+"<br>"+conca1);
+
+
+// var $ali = "ali";
+// alert($ali);
+
+
